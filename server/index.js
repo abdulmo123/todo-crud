b83a076923aee1be9b34ef726af33b9bf564afea
Abdullahi Mohamed <amohamed2096@sdsu.edu>
2022-02-19 06:51:52 -0800

    adding env variables

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const todoController = require('./controllers/todo.controller');
+const cors = require('cors');
+require('dotenv').config();
+// const TaskModel = require('./models/tasks');
 
+app.use(express.json());
 // database connection
-mongoose.connect("mongodb+srv://user:admin@cluster0.xnru8.mongodb.net/ToDoDB?retryWrites=true&w=majority", { useNewUrlParser: true });
-app.listen(3001, () => {
+mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true });
+
+app.get('/getTask', todoController.getTask);
+// app.get('/getTask', async (req, res) => {
+//     const task = new TaskModel({ task: "Hi", time: "2020-12-01", status: "false" });
+//     await task.save();
+//     res.send('task added');
+// });
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log('listening on port 3001');
 })
