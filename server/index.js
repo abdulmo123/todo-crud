c494fca0a94014db20b13c96e60362f0485f857f
Abdullahi Mohamed <amohamed2096@sdsu.edu>
2022-02-18 20:02:56 -0800

    app skeleton

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// database connection
+
+app.listen(3001, () => {
+    console.log('listening on port 3000');
+})
