e5347b57219bf0310805eb6779bca26ac84e2bf9
Abdullahi Mohamed <amohamed2096@sdsu.edu>
2022-03-07 09:01:22 -0800

    sending data from back to front + displaying data

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Axios from 'axios';
 function App() {
   const [task, setTask] = useState('');
   const [date, setDate] = useState(new Date());
   const [time, setTime] = useState(new Date().getTime());
+  const [listofTasks, setListOfTasks] = useState([]);
 
   const createTask = () => {
     Axios.post('http://localhost:3001/createTask', { task: task, date: date, time: time })
       .then(() => {
-        alert('it works!')
+        setListOfTasks([...listofTasks], { task: task, date: date, time: time })
       }).catch(() => {
         alert('it failed!');
       });
   };
+  useEffect(() => {
+    Axios.get('http://localhost:3001/readTask')
+      .then((response) => {
+        setListOfTasks(response.data);
+      }).catch(() => {
+        console.log('ERROR!');
+      });
+  }, []);
   return (
     <div className="App">
       <div className="inputs">
@@ -33,6 +42,18 @@ function App() {
           onChange={(event) => setTime(event.target.value)}
         />
         <button onClick={createTask}>Add Task</button>
+      </div>
+
+      <div className="taskList">
+        {listofTasks.map((e) => {
+          return (
+            <div className="task">
+              <h3>Task: {e.task}</h3>
+              <h3>Date: {e.date}</h3>
+              <h3>Time: {e.time}</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
